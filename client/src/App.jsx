edddff52aad7226437d2fef728e58472c32926f7
Jack Kennedy <jackkennedy190@gmail.com>
2023-10-06 14:42:28 +0400

    create and run server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { BrowerRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import SignUp from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+import About from './pages/About';
+import Header from "./components/header";
 
 export default function App() {
   return (
-    <BrowerRouter>
+    <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
@@ -15,6 +17,6 @@ export default function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowerRouter>
+    </BrowserRouter>
   );
 }
