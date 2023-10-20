97819ca87d257295ddd35ddb34e2e36984c8dfa1
Jack Kennedy <jackkennedy190@gmail.com>
2023-10-20 19:12:48 +0400

    create sign up api rute

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouteer from "./routes/auth.route.js";
+import bodyParser from "body-parser";
 dotenv.config();
 
 mongoose
@@ -12,12 +14,15 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+  
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
 
-app.use('/api/user', userRouter)
+app.use(bodyParser.json());
+app.use(express.json());
 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouteer);
